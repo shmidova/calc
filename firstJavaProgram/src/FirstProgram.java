d987a35c8b781fdfdfe793a5f00bd629a59fec7d
Mariya Shmidova <mshmidova@gmail.com>
2022-09-08 09:32:18 +0300

    2 функции в calc

--- firstJavaProgram/src/FirstProgram.java
+++ firstJavaProgram/src/FirstProgram.java
@@ -3,32 +3,56 @@ import java.util.function.BinaryOperator;
 public class FirstProgram {
     public static void main(String[] args) {
 
-        calc(20, 6,'-');
-        calc(20, 6,'+');
-        calc(20, 6,'*');
-        calc(20, 6,'/');
-        calc(20, 6,'@');
+        ifcalc(20, 6, '-');
+        ifcalc(20, 6, '+');
+        ifcalc(20, 6, '*');
+        ifcalc(20, 6, '/');
+        ifcalc(20, 6, '@');
+
+        switchcalc(20, 6, '-');
+        switchcalc(20, 6, '+');
+        switchcalc(20, 6, '*');
+        switchcalc(20, 6, '/');
+        switchcalc(20, 6, '@');
+
+
     }
 
 
-    public static void calc(double paramA, double paramB, char operation) {
-        if (operation =='+') {
+    public static void ifcalc(double paramA, double paramB, char operation) {
+        if (operation == '+') {
             System.out.println(paramA + paramB);
-        }
-        else if (operation =='-') {
-          System.out.println(paramA - paramB);
-        }
-        else if (operation == '*') {
+        } else if (operation == '-') {
+            System.out.println(paramA - paramB);
+        } else if (operation == '*') {
             System.out.println(paramA * paramB);
-        }
-        else if (operation =='/'){
+        } else if (operation == '/') {
             System.out.println(paramA / paramB);
 
-        }
-        else  {
+        } else {
             System.out.println("Incorrect operation");
         }
     }
 
+    public static void switchcalc(double paramA, double paramB, char operation) {
+        double result = 0;
+        switch (operation) {
+            case '+':
+                result = paramA + paramB;
+                break;
+            case '-':
+                result = paramA - paramB;
+                break;
+            case '*':
+                result = paramA * paramB;
+                break;
+            case '/':
+                result = paramA / paramB;
+                break;
+            default:
+                System.out.println("Incorrect result");
+        }
+        System.out.println(result);
+    }
 
 }
